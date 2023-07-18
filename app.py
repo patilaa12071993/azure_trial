5ebc720f200bffe56d530737d289ff2eea3862fc
patilaa12071993 <85424730+patilaa12071993@users.noreply.github.com>
2023-07-18 23:41:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,17 @@ from flask import (Flask, redirect, render_template, request,
 app = Flask(__name__)
 
 
-@app.route('/impact', methods=['GET','POST'])
+@app.route('/', methods=['GET','POST'])
 def impact():
-   name = request.form.get('name')
+  return "Hello"
+   # name = request.form.get('name')
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return name
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return None
+   # if name:
+   #     print('Request for hello page received with name=%s' % name)
+   #     return name
+   # else:
+   #     print('Request for hello page received with no name or blank name -- redirecting')
+   #     return None
 
 
 if __name__ == '__main__':
